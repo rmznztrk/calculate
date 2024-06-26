fd48064aa6b0fd7915bf6a94c75c9867102fac71
ramazan <rmzn_3766@hotmail.com>
2024-06-27 00:22:27 +0300

    edited on and off buttons

--- script.js
+++ script.js
@@ -12,29 +12,37 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  // getElement type-1:
-  function setOn() {
-    const btnOn = document.getElementById("btn-on");
+  const btnOn = document.getElementById("btn-on");
+  const btnOff = document.getElementById("btn-off");
 
+  function setOn() {
     isOnBtnActive = !isOnBtnActive;
     console.log("OnBtn: " + isOnBtnActive);
-    isOnBtnActive
-      ? (btnOn.style.backgroundColor = "rgb(0, 205, 0)")
-      : (btnOn.style.backgroundColor = "rgb(0, 210, 0, 0.450)");
+    if (isOnBtnActive) {
+      isOffBtnActive = false;
+      btnOn.style.backgroundColor = "rgb(0, 205, 0)";
+      btnOff.style.backgroundColor = "rgb(255, 0, 0, 0.562)";
+    } else {
+      btnOn.style.backgroundColor = "rgb(0, 210, 0, 0.450)";
+      btnOff.style.backgroundColor = "rgb(255, 0, 0)";
+    }
   }
 
-  // getElement type-2:
-  document.getElementById("btn-off").addEventListener("click", function () {
-    // Butonun arka plan rengini değiştir
+  function setOff() {
     isOffBtnActive = !isOffBtnActive;
     console.log("OffBtn: " + isOffBtnActive);
-    isOffBtnActive
-      ? (this.style.backgroundColor = "rgb(255, 0, 0)")
-      : (this.style.backgroundColor = "rgb(255, 0, 0, 0.562)");
-  });
+    if (isOffBtnActive) {
+      isOnBtnActive=false;
+      btnOff.style.backgroundColor = "rgb(255, 0, 0)";
+      btnOn.style.backgroundColor = "rgb(0, 210, 0, 0.450)";
+    } else {
+      btnOff.style.backgroundColor = "rgb(255, 0, 0, 0.562)";
+      btnOn.style.backgroundColor = "rgb(0, 205, 0)";
+    }
+  }
 
   // `writeToScreen` ve `setOn` fonksiyonlarını global yaparak HTML'deki `onclick` işlevselliğini koruyalım
   window.writeToScreen = writeToScreen;
   window.setOn = setOn;
-  
+  window.setOff = setOff;
 });
